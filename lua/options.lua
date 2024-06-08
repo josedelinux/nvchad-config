945e9242284311b912b20dbf55b421d71c78f1d1
Juan Chan <josedelinux@hotmail.com>
2024-06-08 23:17:05 +0800

    add powershell support

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,20 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+local use_pwsh = true -- weather to enable pwsh or not
+
+local is_windows = vim.fn.has "win32" ~= 0
+if use_pwsh and is_windows then
+  -- ref: https://neovim.io/doc/user/options.html#'shell'
+  o.shell = vim.fn.executable "pwsh" and "pwsh" or "powershell"
+  o.shellcmdflag =
+    "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
+  o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+  o.shellpipe = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+  o.shellquote = ""
+  o.shellxquote = ""
+end
+
