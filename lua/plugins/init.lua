c9feb4b3c4127c556a5c7a3fdece5a3eaf2e8ac7
Juan Chan <josedelinux@hotmail.com>
2024-06-09 14:58:35 +0800

    adjust structure

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,4 @@
+-- default plugins
 return {
   {
     "stevearc/conform.nvim",
@@ -7,32 +8,112 @@ return {
     end,
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+
+        "clangd",
+        "clang-format",
+
+        "gopls",
+        "goimports",
+
+        "pyright",
+        "ruff",
+        "mypy",
+        -- ya que tenemos `ruff`
+        -- "black",
+        -- "isort",
+
+        "typescript-language-server",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+
+        "codespell",
+      },
+    },
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false, -- Toggle with `:Gitsigns toggle_linehl` -- it could be harder to find out
+      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff` -- why no space ph?
+
+      -- also be aware that you can always use space gb(Git Blame)
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 100, -- defaults: 1000
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults
+        "vim",
+        "lua",
+        "vimdoc",
+
+        -- web dev
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+
+        -- low level
+        "c",
+        "cpp",
+        "rust",
+
+        -- go
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+
+        -- python
+        "python",
+
+        -- build system
+        "make",
+        "cmake",
+
+        -- shells
+        "bash",
+        "fish",
+
+        -- markdown
+        "markdown",
+        "markdown_inline",
+
+        -- git
+        "git_config",
+        "gitcommit",
+        "git_rebase",
+        "gitignore",
+        "gitattributes",
+      },
+    },
+  },
 }
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -5,114 +5,6 @@
 ---@type NvPluginSpec[]
 local plugins = {
   {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
-    end,
-  },
-
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "stylua",
-
-        "clangd",
-        "clang-format",
-
-        "gopls",
-        "goimports",
-
-        "pyright",
-        "ruff",
-        -- ya que tenemos `ruff`
-        -- "black",
-        -- "isort",
-
-        "typescript-language-server",
-        "html-lsp",
-        "css-lsp",
-        "prettier",
-
-        "codespell",
-      },
-    },
-  },
-
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
-      linehl = false, -- Toggle with `:Gitsigns toggle_linehl` -- it could be harder to find out
-      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff` -- why no space ph?
-
-      -- also be aware that you can always use space gb(Git Blame)
-      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-      current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-        delay = 100, -- defaults: 1000
-        ignore_whitespace = false,
-        virt_text_priority = 100,
-      },
-    },
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        -- defaults
-        "vim",
-        "lua",
-        "vimdoc",
-
-        -- web dev
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "tsx",
-
-        -- low level
-        "c",
-        "cpp",
-        "rust",
-
-        -- go
-        "go",
-        "gomod",
-        "gowork",
-        "gosum",
-
-        -- python
-        "python",
-
-        -- build system
-        "make",
-        "cmake",
-
-        -- shells
-        "bash",
-        "fish",
-
-        -- markdown
-        "markdown",
-        "markdown_inline",
-
-        -- git
-        "git_config",
-        "gitcommit",
-        "git_rebase",
-        "gitignore",
-        "gitattributes",
-      },
-    },
-  },
-
-  {
     "RRethy/vim-illuminate",
     lazy = false,
     config = function()
@@ -120,7 +12,7 @@ local plugins = {
         large_file_cutoff = 10000,
       }
     end,
-    -- enabled = false, -- This plugin causes freezing when opening big files
+    enabled = false, -- This plugin causes freezing when opening big files
   },
 }
 
